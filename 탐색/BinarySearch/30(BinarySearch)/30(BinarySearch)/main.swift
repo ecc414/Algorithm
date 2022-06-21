577c385bcbc02bbeb80910d6416c17ea8959fdaa
ecc414 <ecc00414@gmail.com>
2022-06-21 19:58:56 +0900

    no message

--- 탐색/BinarySearch/30(BinarySearch)/30(BinarySearch)/main.swift
+++ 탐색/BinarySearch/30(BinarySearch)/30(BinarySearch)/main.swift
@@ -7,5 +7,34 @@
 
 import Foundation
 
-print("Hello, World!")
+let input         = readLine()!.split(separator:" ").map{Int(String($0))!}
+let (numOfLectures,min) = (input[0],input[1])
+let lectures      = readLine()!.split(separator: " ").map{Int(String($0))!}
+//binarySearch
+var start = lectures.max()!     // !!!!!!!! lectures[numOfLectures - 1]해서 계속 틀림
+var end   = lectures.reduce(0,+)
+while(start<end){
+    let mid   = (start + end) / 2
+    var sum = 0
+    //var count = 0
+    var count = 1
+    for lecture in lectures{
+        if sum + lecture > mid{
+            sum = lecture
+            count += 1
+        }else{
+            sum += lecture
+        }
+    }
+//    if sum != 0 {
+//        count += 1
+//    }
+    if count > min {
+        start = mid + 1
+    }else{
+        end   = mid
+    }
+    print("start : \(start), end : \(end), mid : \(mid), count : \(count)")
+}
 
+print(start)
--- 탐색/BinarySearch/31(BinarySearch)/31(BinarySearch)/main.swift
+++ 탐색/BinarySearch/31(BinarySearch)/31(BinarySearch)/main.swift
@@ -7,5 +7,23 @@
 
 import Foundation
 
-print("Hello, World!")
-
+let n = Int(String(readLine()!))!    //size of array
+let k = Int(String(readLine()!))!    //target!
+    
+var start = 1
+var end   = n*n
+var answer = 0
+while(start<=end){
+    let mid = (start + end) / 2
+    var count = 0
+    for i in 1...n{
+        count += min( mid / i , n )
+    }
+    if count < k {
+        start = mid + 1
+    }else{
+        end   = mid - 1
+        answer = mid
+    }
+}
+print(answer)
